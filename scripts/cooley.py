9390963d3338a604846350ae1dc338b834047d19
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-03-28 21:54:55 -0500

    generate files to temporary directory (and some cleanup)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/cooley.py
+++ scripts/cooley.py
@@ -2,31 +2,31 @@ import argparse
 import sys
 import os
 import os.path
-import cooley_include as templates
+import tempfile
 
-generated = os.path.join("..", "generated")
-if os.path.exists(generated):
-   print("error: %s exists. Please delete and re-run." % generated)
-   sys.exit(1)
 
-os.makedirs(generated)
+import tempfile
+generated = tempfile.mkdtemp('.d', 'qsubs.', os.getcwd())
+print("Scripts will be written to dir %s " % generated)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dims', type=int, nargs='+', default=[128, 256])
 parser.add_argument('--nodes', type=int, nargs='+', default=[ 2**n for n in range(0, 6) ])
 parser.add_argument('--cores', type=int, nargs='+', default=[12])
+parser.add_argument('--add_workload', type=int, nargs='+', default=[])
 options = parser.parse_args()
 
-GEN_FILENAME = "%(generated)s/run-n%(nodes)s-p%(partitions)s-w%(workload)s-dim%(dim)s.sh"
+from cooley_include import *
 
 for dim in options.dims:
    for nodes in options.nodes:
       for cores in options.cores:
         partitions = nodes * cores
         workload = partitions
-        filename = GEN_FILENAME % vars()
-        with open(filename, "w") as outfile:
-            print("Creating script %s" % filename)
-            outfile.write(templates.SCRIPT_HEADER)
-            outfile.write(templates.SPARK_INIT)
-            outfile.write(templates.SPARK_SUBMIT % vars())
+        all_workloads = [ workload ] + options.add_workload
+        for workload in all_workloads:
+           filename = SCRIPT_FILENAME % vars()
+           script_code = SCRIPT_HEADER + SPARK_INIT + SPARK_SUBMIT
+           with open(filename, "w") as outfile:
+               outfile.write(script_code % vars())
 
